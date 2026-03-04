919b366aaa669b57783e0560ae550d97861a8e0d
Tasnim Kabir <90052219+MTKabir@users.noreply.github.com>
2026-03-04 10:57:30 +0100

    added custom logger

--- monitor.py
+++ monitor.py
@@ -1,25 +1,29 @@
-import schedule,os,logging
-from datetime import time,datetime
+import schedule,os,logging,time
+from datetime import datetime
 
-logging.basicConfig(filename="logs.log",filemode="w", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 INCOMING_FOLDER = "./incoming"
 PROCESSED_FOLDER = "./processed"
-# first i will go inside the incoming folder and get all the csv files(though according to the requirement i expect only one file just incase there are more than one file) 
-# and then i will read the csv file and then move the file to the processed folder
+# create our custom logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler("logs.log")
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)  
 
 def job():
     csv_files = [file for file in os.listdir(INCOMING_FOLDER) if file.endswith(".csv")]
     
     if len(csv_files) == 0:
-        logging.info("no csv file in the incoming folder")
+        logger.info("No CSV file found in incoming folder")
     elif len(csv_files) == 1:
         filename = csv_files[0]
         source_path = os.path.join(INCOMING_FOLDER, filename)
         destination_path = os.path.join(PROCESSED_FOLDER, filename)
         os.rename(source_path, destination_path)
-        logging.info("csv file moved to the processed folder")
+        logger.info("csv file moved to the processed folder")
     else:
-        logging.info("more than one csv file in the incoming folder which is not allowed")
+        logger.info("more than one csv file in the incoming folder which is not allowed")
         
 def write_log(message):
     with open("log.txt", "a") as log_file:
@@ -29,3 +33,4 @@ schedule.every().seconds.do(job)
 
 while True:
     schedule.run_pending()
+    time.sleep(1)
